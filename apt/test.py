a9c2f1214a71501ad1fea268ec45e9a65dc2fd8b
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-03 14:55:09 +0700

    update

---
+++ apt/test.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import requests
+from transformers import AutoProcessor, BlipForImageTextRetrieval
+
+model = BlipForImageTextRetrieval.from_pretrained("Salesforce/blip-itm-base-coco")
+processor = AutoProcessor.from_pretrained("Salesforce/blip-itm-base-coco")
+
+url = "/home/khoahocmaytinh2022/Desktop/MinhNhut/DATA/caltech-101/101_ObjectCategories/accordion/image_0001.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+text = "an image of a accordion"
+
+inputs = processor(images=image, text=text, return_tensors="pt")
+outputs = model(**inputs)
