2e5629456874e23a9ef764fe6e88d6efee24dcf7
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-03 14:57:52 +0700

    update

--- apt/test.py
+++ apt/test.py
@@ -5,8 +5,8 @@ from transformers import AutoProcessor, BlipForImageTextRetrieval
 model = BlipForImageTextRetrieval.from_pretrained("Salesforce/blip-itm-base-coco")
 processor = AutoProcessor.from_pretrained("Salesforce/blip-itm-base-coco")
 
-url = "./DATA/caltech-101/101_ObjectCategories/accordion/image_0001.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+path = "./DATA/caltech-101/101_ObjectCategories/accordion/image_0001.jpg"
+image = Image.open(path)
 text = "an image of a accordion"
 
 inputs = processor(images=image, text=text, return_tensors="pt")
