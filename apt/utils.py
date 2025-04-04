e7e0da1d7bb5871890d450b1d3e48fb1770a228f
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-04 10:31:38 +0700

    update

--- apt/utils.py
+++ apt/utils.py
@@ -289,7 +289,8 @@ def accuracy(output, target, topk=(1,)):
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
-        print(pred, target)
+        print('predict: ', pred)
+        print('target:', target)
         res = []
         for k in topk:
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
