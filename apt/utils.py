b223e808b85b30963492dfa1cce308e14040c660
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-04 09:46:41 +0700

    update

--- apt/utils.py
+++ apt/utils.py
@@ -221,8 +221,9 @@ class CustomBLIP(nn.Module):
     def forward(self, image):
         inputs = self.processor(images=image, return_tensors="pt", padding=True)
         # pixel_values = inputs.pixel_values.cuda()
+        inputs = {k: v.cuda() for k, v in inputs.items()}
         vision_outputs = self.model.vision_model(
-            **inputs.cuda(),
+            **inputs,
             # pixel_values=pixel_values,
             # output_attentions=output_attentions,
             # output_hidden_states=output_hidden_states,
