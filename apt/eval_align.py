ccc23e97ea3b03ab24ad9e684644212a70ee9083
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-08 14:31:30 +0700

    update

--- apt/eval_align.py
+++ apt/eval_align.py
@@ -249,26 +249,26 @@ if __name__ == '__main__':
         acc = accuracy(output, tgts)
         meters.acc.update(acc[0].item(), bs)
 
-        model.mode = 'attack'
-        if args.attack == 'aa':
-            adv = attack.run_standard_evaluation(imgs, tgts, bs=bs)
-        elif args.attack in ['pgd', 'tpgd']:
-            images = [ToPILImage()(img.float().cpu()) for img in imgs]  # từ batch ảnh raw
-            processed = model.processor(images=images, return_tensors="pt")
-            pixel_values = processed["pixel_values"].cuda()
-            pixel_values.requires_grad_()
-            adv = attack(pixel_values, tgts)
-        else:
-            adv, _ = pgd(imgs, tgts, model, CWLoss, eps, alpha, steps)
+        # model.mode = 'attack'
+        # if args.attack == 'aa':
+        #     adv = attack.run_standard_evaluation(imgs, tgts, bs=bs)
+        # elif args.attack in ['pgd', 'tpgd']:
+        #     images = [ToPILImage()(img.float().cpu()) for img in imgs]  # từ batch ảnh raw
+        #     processed = model.processor(images=images, return_tensors="pt")
+        #     pixel_values = processed["pixel_values"].cuda()
+        #     pixel_values.requires_grad_()
+        #     adv = attack(pixel_values, tgts)
+        # else:
+        #     adv, _ = pgd(imgs, tgts, model, CWLoss, eps, alpha, steps)
             
-        model.mode = 'classification'
+        # model.mode = 'classification'
 
-        # Calculate features
-        with torch.no_grad():
-            output = model(adv)
+        # # Calculate features
+        # with torch.no_grad():
+        #     output = model(adv)
 
-        rob = accuracy(output, tgts)
-        meters.rob.update(rob[0].item(), bs)
+        # rob = accuracy(output, tgts)
+        # meters.rob.update(rob[0].item(), bs)
 
         if i == 1 or i % 10 == 0 or i == len(loader):
             progress.display(i)
