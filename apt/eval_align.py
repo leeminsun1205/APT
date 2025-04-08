46b394bb7b65066e38031a7acf8eaf4f197d3e0b
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-08 14:19:07 +0700

    udpate

--- apt/eval_align.py
+++ apt/eval_align.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
         ])
         testset = CIFAR10(root='./data', transform=transformer, train=False, download=True)
         loader = DataLoader(testset,
-                       batch_size=100,
+                       batch_size=8,
                        num_workers=8,
                        sampler=SequentialSampler(testset),)
 
