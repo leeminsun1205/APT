477292d337d31c4fe61ddaf9d9c48ee9fc975ebd
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-08 09:31:58 +0700

    update

--- apt/utils.py
+++ apt/utils.py
@@ -279,7 +279,7 @@ class CustomALIGN(nn.Module):
             return_tensors="pt",
             padding=True
         ).to(images.device)
-        print(inputs)
+        print(dir(inputs))
         if self.mode == 'classification':
             # Get model outputs
             with torch.no_grad():
