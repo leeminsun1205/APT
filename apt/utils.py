438ce056ea18d5f9bafe5aa563584b38b0556154
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-09 16:51:20 +0700

    update

--- apt/utils.py
+++ apt/utils.py
@@ -197,13 +197,13 @@ class CustomBLIP(BaseCustomModel):
         # if self.mode == 'attack':
         image_embeds = self.model.visual_encoder.forward_features(images)
         image_feats = self.model.vision_proj(image_embeds)
-        image_feats = F.normalize(image_feats, dim=-1).image_embeds_proj[:, 0]
+        image_feats = F.normalize(image_feats, dim=-1)
         # else:
         #     image_inputs = {"image": images}
         #     image_feats = self.model.extract_features(image_inputs, mode="image").image_embeds_proj[:, 0]        
         image_feats = image_feats / image_feats.norm(dim=-1, keepdim=True)
         text_feats = self.cls_tfeatures if self.mode == 'classification' else self.atk_tfeatures
-        return image_feats @ text_feats.T 
+        return image_feats @ text_feats.t()
 
 
 class CustomALIGN(BaseCustomModel):
