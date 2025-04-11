a4d1c8d055b86f54fe0eed4e780b29b6b8212458
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-11 09:12:46 +0700

    update

--- apt/eval_clip.py
+++ apt/eval_clip.py
@@ -186,7 +186,6 @@ if __name__ == '__main__':
 
     model, _ = clip.load(cfg.MODEL.BACKBONE.NAME, device='cpu')
 
-    load pretrained adversarially robust backbone models
     ckp_name = 'vitb32' if cfg.MODEL.BACKBONE.NAME == 'ViT-B/32' else 'rn50'
     eps = int(cfg.AT.EPS * 255)
     # eps = 
