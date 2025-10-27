24d23ee160017f09f4e68d750a00c7567c96184c
leeminsun1205 <minhnhut12052004@gmai.com>
2025-10-27 15:01:57 +0700

    update

--- apt/scripts/APT.sh
+++ apt/scripts/APT.sh
@@ -53,6 +53,15 @@ else
     --dataset-config-file configs/datasets/${DATASET}.yaml \
     --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
     --output-dir ${DIR} \
-    TRAIN.CHECKPOINT_FREQ 1 \
-    "$@"  # <--- CHỈ GIỮ LẠI DÒNG NÀY
+    --eps ${EPS} \
+    --alpha ${ALPHA} \
+    --steps ${STEPS} \
+    --adv-prompt ${ATP} \
+    --prompt-alpha ${PALPHA} \
+    --resume "${RESUME}" \
+    TRAINER.COOP.N_CTX ${NCTX} \
+    TRAINER.COOP.CSC ${CSC} \
+    TRAINER.COOP.CLASS_TOKEN_POSITION ${CTP} \
+    DATASET.NUM_SHOTS ${SHOTS} \
+    TRAIN.CHECKPOINT_FREQ 1 
 fi
