7eeaff3af756c5ca55ee5661d2f0f4e8333dba99
leeminsun1205 <minhnhut12052004@gmai.com>
2025-10-27 15:08:57 +0700

    update

--- apt/scripts/APT.sh
+++ apt/scripts/APT.sh
@@ -4,48 +4,50 @@
 DATA=kaggle/input/
 TRAINER=APT
 
+# 1. Đọc 13 tham số vị trí
 DATASET=$1
-CFG=$2  # config file
-CTP=$3  # class token position (end or middle)
-NCTX=$4  # number of context tokens
-SHOTS=$5  # number of shots (1, 2, 4, 8, 16)
-CSC=$6  # class-specific context (False or True)
-EPS=$7  # epsilon for AT
-ALPHA=$8  # alpha or step size for AT
-STEPS=$9  # number of steps for AT
+CFG=$2
+CTP=$3
+NCTX=$4
+SHOTS=$5
+CSC=$6
+EPS=$7
+ALPHA=$8
+STEPS=$9
 SEED=${10}
 ATP=${11}
 PALPHA=${12}
+RESUME=${13} # <--- $13 là đường dẫn resume
+
+# 2. Kiểm tra resume (dựa trên biến $13, không phải "$@")
 IS_RESUME=false
-for arg in "$@"; do
-    if [[ "$arg" == "--resume" ]]; then
-        IS_RESUME=true
-        break
-    fi
-done
-RESUME=${13}
+if [ -n "$RESUME" ]; then
+    IS_RESUME=true
+fi
 
+# 3. Lấy TẤT CẢ các tham số "thêm" (từ $14 trở đi)
+# Đây chính là cờ --no-backbone của bạn
+EXTRA_ARGS="${@:14}"
 
+# 4. Tạo đường dẫn DIR (Logic này của bạn đã đúng)
 #pertubed
 if [ ${ATP} == 'perturbed' ]
 then
     DIR=output/${DATASET}/${TRAINER}/${CFG}_${SHOTS}shots/nctx${NCTX}_csc${CSC}_ctp${CTP}/eps${EPS}_alpha${ALPHA}_step${STEPS}_${ATP}_${PALPHA}/seed${SEED}
-
 #constant
 elif [ ${ATP} == 'constant' ]
 then
     DIR=output/${DATASET}/${TRAINER}/${CFG}_${SHOTS}shots/nctx${NCTX}_csc${CSC}_ctp${CTP}/eps${EPS}_alpha${ALPHA}_step${STEPS}_${ATP}/seed${SEED}
-
 #onfly
 else
     DIR=output/${DATASET}/${TRAINER}/${CFG}_${SHOTS}shots/nctx${NCTX}_csc${CSC}_ctp${CTP}/eps${EPS}_alpha${ALPHA}_step${STEPS}/seed${SEED}
 fi
 
-#results existed
+# 5. Kiểm tra thư mục (Logic này của bạn đã đúng)
 if [ "$IS_RESUME" = false ] && [ -d "$DIR" ]; then
     echo "Oops! The results exist at ${DIR} (so skip this job)"
 
-#train is implemented
+# 6. Chạy python (truyền tham số từ biến)
 else
     python train.py \
     --root ${DATA} \
@@ -53,6 +55,16 @@ else
     --dataset-config-file configs/datasets/${DATASET}.yaml \
     --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
     --output-dir ${DIR} \
+    --eps ${EPS} \
+    --alpha ${ALPHA} \
+    --steps ${STEPS} \
+    --adv-prompt ${ATP} \
+    --prompt-alpha ${PALPHA} \
+    --resume "${RESUME}" \
+    TRAINER.COOP.N_CTX ${NCTX} \
+    TRAINER.COOP.CSC ${CSC} \
+    TRAINER.COOP.CLASS_TOKEN_POSITION ${CTP} \
+    DATASET.NUM_SHOTS ${SHOTS} \
     TRAIN.CHECKPOINT_FREQ 1 \
-    "$@"  # <-- Dòng này sẽ nhận TẤT CẢ cờ (flag) từ notebook
+    ${EXTRA_ARGS} # <--- Truyền các cờ "thêm" (như --no-backbone) vào đây
 fi
