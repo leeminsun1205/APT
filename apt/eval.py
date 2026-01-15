1539b9bea3c6991e43ab77d60cdc580e4593e044
leeminsun1205 <minhnhut12052004@gmai.com>
2026-01-15 09:39:30 +0700

    update load backbone eps

--- apt/eval.py
+++ apt/eval.py
@@ -261,7 +261,8 @@ if __name__ == '__main__':
 
         ckp = torch.load(
             os.path.join('backbone', ckp_name),
-            map_location='cpu'
+            map_location='cpu',
+            weights_only=True
         )
 
         state = ckp['vision_encoder_state_dict']
